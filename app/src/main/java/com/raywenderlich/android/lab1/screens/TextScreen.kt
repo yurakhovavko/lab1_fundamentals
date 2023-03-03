2421d165af22a3207f40641c73c95208318b4d82
Yura <khovavko1@gmail.com>
2023-03-03 19:30:11 +0300

    ЖесткаяНадпись

--- app/src/main/java/com/raywenderlich/android/lab1/screens/TextScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/TextScreen.kt
@@ -1,11 +1,18 @@
 package com.raywenderlich.android.lab1.screens
 
+import android.graphics.fonts.FontStyle
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.raywenderlich.android.lab1.R
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
@@ -27,5 +34,12 @@ fun TextScreen() {
 
 @Composable
 fun MyText() {
-    //TODO add your code here
+    Text(
+        text= stringResource(id = R.string.set_text),
+        color = colorResource(id = R.color.green),
+        fontStyle = androidx.compose.ui.text.font.FontStyle.Italic,
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold
+
+    )
 }
