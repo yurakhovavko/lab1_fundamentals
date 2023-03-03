b6331265f5255c8fdd2574303a08e399b2b3d4f1
Yura <khovavko1@gmail.com>
2023-03-03 20:30:20 +0300

    ProgressIndicator

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ProgressIndicatorsScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ProgressIndicatorsScreen.kt
@@ -3,9 +3,13 @@ package com.raywenderlich.android.lab1.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
@@ -18,7 +22,11 @@ fun ProgressIndicatorScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        //TODO add your code here
+        CircularProgressIndicator(
+            color = colorResource(id = androidx.compose.ui.R.color.vector_tint_theme_color),
+            strokeWidth = 5.dp
+        )
+        LinearProgressIndicator(progress = 0.5f)
     }
 
     BackButtonHandler {
