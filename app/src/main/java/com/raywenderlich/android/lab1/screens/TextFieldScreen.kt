d56b13aae6fa7c834df7ea24f8a06642dfa066ad
Yura <khovavko1@gmail.com>
2023-03-03 19:49:34 +0300

    TextField

--- app/src/main/java/com/raywenderlich/android/lab1/screens/TextFieldScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/TextFieldScreen.kt
@@ -3,9 +3,20 @@ package com.raywenderlich.android.lab1.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import com.raywenderlich.android.lab1.R
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
@@ -27,5 +38,23 @@ fun TextFieldScreen() {
 
 @Composable
 fun MyTextField() {
-    //TODO add your code here
+      val textValue = remember { mutableStateOf("") }
+    val primaryColor = colorResource(id = R.color.green)
+
+    OutlinedTextField(
+
+        label = { Text(text = stringResource(id = R.string.insert_text)) },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedLabelColor = primaryColor,
+            focusedBorderColor = primaryColor,
+            cursorColor = primaryColor
+
+        ),
+        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email),
+        value = textValue.value,
+        onValueChange = {textValue.value = it},
+
+    )
+
+
 }
